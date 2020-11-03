3ad2e094bfc7390c13b302f182ba5e674375c9a8
Martin RA <marpht23@gmail.com>
2020-11-03 18:02:15 +0100

    some helpful printing

--- src/generate_schedule.py
+++ src/generate_schedule.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 from datetime import datetime, timedelta
 from helper_functions import load_json_from_file, construct_day_groups, save_json_file_from_dict
+import os
+
+# see if data dir is here
+if os.path.exists("../data") is False:
+    print("create data dir in parent dir, and ask Martin for people.json file (I didn't want to put it in git)")
+    exit()
 
 # load people.json
 people = load_json_from_file("../data/people.json")
@@ -14,3 +20,4 @@ schedule = list(map(lambda dt: construct_day_groups(dt, people), dateList))
 
 # save groups into schedule.json file
 save_json_file_from_dict(schedule, "../data/schedule.json")
+print("schedule succesfully generated and put here: " + os.path.abspath("../data/schedule.json"))

48d1adee48d32269de9ccbb55e358fa63abd6345
Martin RA <marpht23@gmail.com>
2020-11-03 18:43:09 +0100

    copy static files

--- src/generate_frontend.py
+++ src/generate_frontend.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+from distutils.dir_util import copy_tree
 from jinja2 import Environment, select_autoescape, FileSystemLoader
 from helper_functions import load_json_from_file, write_string_to_file
 
@@ -16,11 +17,11 @@ schedule = load_json_from_file("../data/schedule.json")
 # load jinja template
 template = env.get_template('index.html')
 
+# render outpat based on schedule dict
 output = template.render(schedule=schedule)
-
-
 
 if os.path.exists("../dist") is False:
     os.mkdir("../dist")
 write_string_to_file(output, "../dist/index.html")
+copy_tree("templates/static", "../dist/static") # copy static files
 print("index.html successfully generated and put here: " + os.path.abspath("../dist/index.html"))
